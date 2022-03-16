d5899fc3280c228fa6badd96c72a0fb62603b47e
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-16 15:09:23 -0300

    Primeiro Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module lord.sistema.login.sistemadelogin {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+
+    opens lord.sistema.login.sistemadelogin to javafx.fxml;
+    exports lord.sistema.login.sistemadelogin;
+}
