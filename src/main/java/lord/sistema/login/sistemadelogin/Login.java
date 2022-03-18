1e7f6bc8665909482a01487039c7eef6e5535a89
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-18 09:28:38 -0300

    Editando o titulo da aplicação

--- src/main/java/lord/sistema/login/sistemadelogin/Login.java
+++ src/main/java/lord/sistema/login/sistemadelogin/Login.java
@@ -3,7 +3,9 @@ package lord.sistema.login.sistemadelogin;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import lord.sistema.login.sistemadelogin.gui.utilitarios.Alertas;
 
 import java.io.IOException;
 
@@ -12,7 +14,7 @@ public class Login extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +22,5 @@ public class Login extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
