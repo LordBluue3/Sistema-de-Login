2585beae92051a1539b2f3bcb47f626e15a6b9c5
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-25 13:02:35 -0300

    Conectando e enviando mensagens para o ServerSocket

--- src/main/java/lord/sistema/login/sistemadelogin/controlador/ViewController.java
+++ src/main/java/lord/sistema/login/sistemadelogin/controlador/ViewController.java
@@ -6,8 +6,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lord.sistema.login.sistemadelogin.gui.utilitarios.Alertas;
+import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
 import org.json.simple.JSONObject;
 
+import javax.xml.transform.Source;
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -26,21 +32,50 @@ public class ViewController {
     private int contagem = 1;
 
     @FXML
+    private String porta = "12345";
+
+    @FXML
+    private String servidor = "127.0.0.1";
+
+    @FXML
         public void onBtnLoginAction () {
 
+        JSONObject json = new JSONObject();
+        String login = txtLogin.getText();
+        String senha = txtSenha.getText();
+
+        json.put("login", txtLogin.getText());
+        json.put("senha", txtSenha.getText());
+
+        try {
+            // need host and port, we want to connect to the ServerSocket at port 7777
+            Socket socket = new Socket(servidor, Integer.parseInt(porta));
+            System.out.println("Connected!");
+
+            // get the output stream from the socket.
+            OutputStream outputStream = socket.getOutputStream();
+            // create an object output stream from the output stream so we can send an object through it
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+            // make a bunch of messages to send.
+            List<String> messages = new ArrayList<>();
+
+            messages.add(new String(json.toJSONString()));
+
+
+            System.out.println("Sending messages to the ServerSocket");
+            objectOutputStream.writeObject(messages);
+
+            System.out.println("Closing socket and terminating program.");
+            socket.close();
+
+        } catch (IOException e) {
+         e.printStackTrace();
+        }
         //Login e senha
         String loginn = "Lord@gmail.com";
         String senhaa = "1234";
 
-            // Pegando o login e senha que o usuario digitar no textField
-            JSONObject json = new JSONObject();
-
-            String login = txtLogin.getText();
-            String senha = txtSenha.getText();
-
-            json.put("email",login);
-            json.put("senha",senha);
-            System.out.println(json);
 
             //contador para se a pessoa errar a senha 3x fechar a aplicação
             if(contagem == 3 ){
